c121fe7c4fdc5a63a29bc54388b47ebf7e482646
giveuper39 <giveuper39@gmail.com>
2023-11-04 21:37:32 +0300

    finally base template is working

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("catalogue/", views.catalogue, name="catalogue"),
+    path("generator/", views.generator, name="generator"),
+    path("login/", views.login, name="login")
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,19 @@ from django.http import HttpResponse, HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "main/index.html")
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/about.html")
+
+
+def catalogue(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/catalogue.html")
+
+
+def generator(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/generator.html")
+
+
+def login(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/login.html")
