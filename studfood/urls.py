5e8ebf786093991a865d329d1233f2da23863b34
giveuper39 <giveuper39@gmail.com>
2023-11-04 19:00:26 +0300

    fixed some bad stuff, started to work with htmls

--- studfood/urls.py
+++ studfood/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", include("main.urls")),
     path("admin/", admin.site.urls),
-
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
