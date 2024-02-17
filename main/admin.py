d2e4a71d633da632ae69ca9171f8682b7b1d50fa
Eugene <giveuper39@gmail.com>
2024-02-17 14:50:07 +0300

    changing recipe model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from main.models import Recipe, User, Category
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["email"]
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
--- main/models.py
+++ main/models.py
@@ -5,16 +5,16 @@ from .managers import CustomUserManager
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True, primary_key=True)
-    
+
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=80)
-    description = models.CharField(max_length=200)
+    description = models.TextField(max_length=2000)
     categories = models.ManyToManyField(Category)
-    cooking_time = models.TimeField()
+    cooking_time = models.CharField(max_length=8)
     cost = models.IntegerField()
 
 
